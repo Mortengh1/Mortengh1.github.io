49235d667cff16c8561cd115bafe2231e5d56d65
Morten Hansen <mortengh@live.dk>
2020-09-14 11:44:58 +0200

    Projekt

---
+++ js/js.js
@@ -0,0 +1,20 @@
+/*
+ * fil: js.js
+ * purpose: introdction to jQuery
+ */
+console.log('file: js/js.js loaded');
+
+// A $( document ).ready() block.
+$(document).ready(function () { // kører så snart DOM er klar
+
+	console.log("jQuery 3.5.1 running. Alert level green.");
+
+	// ... min kode herfra ...
+    
+    // Får videoen til at afspille af sig selv
+document.getElementById('myVideo').play();
+       
+    
+	// ... min kode slut ...
+
+}); // denne line må ikke slettes
